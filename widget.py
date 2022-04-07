0ab9810e8a98279da892a07653afd3a6d2e9cbe1
Justin Rosengart <j.rosengart@gmx.de>
2022-04-07 19:01:55 +0200

    changed

--- widget.py
+++ widget.py
@@ -8,11 +8,13 @@ from PySide6.QtGui import *
 from MirrorUI.getnews import GetNews
 from MirrorUI.weather import Weather
 from MirrorUI.datetime import DateTime
+from MirrorUI.getGreeting import Greeting
 
 from time import sleep
+from random import randint
 
 
-class Main(QMainWindow):
+class MirrorUI(QMainWindow):
     def __init__(self):
         super().__init__()
         loader = QUiLoader()
@@ -23,12 +25,20 @@ class Main(QMainWindow):
         image.loadFromData(requests.get(Weather().getIcon()).content)
         pixmap = QPixmap(image)
 
+        self.file1 = open('MirrorUI/assets/txt/greetings.txt', 'r')
+        self.Lines = self.file1.readlines()
+
         self.ui.clock.setText(DateTime().get_time().strftime("%H:%M:%S"))
         self.ui.date.setText(DateTime().get_date().strftime("%d.%m.%Y"))
-        self.ui.greating.setText("GREETING")
+        self.ui.greating.setText(Greeting(self.getName()).getGreeting())
         self.ui.temp.setText(Weather().getTemp())
         self.ui.temp_visual.setPixmap(pixmap)
         self.ui.news.setText("NEWS")
+
+    def getName(self):
+        for count, line in enumerate(self.Lines, start=1):
+            if count == randint(1, len(self.Lines)):
+                return f"{line.strip()} {self.name}"
 
     async def setDateTime(self):
         print("start1")
@@ -47,14 +57,16 @@ class Main(QMainWindow):
             image = QImage()
             image.loadFromData(requests.get(Weather().getIcon()).content)
             pixmap = QPixmap(image)
-            self.ui.temp.setText(Weather().getTemp())
+            self.ui.temp.setText(pixmap)
             
             await asyncio.sleep(30)
 
+
 if __name__ == "__main__":
     app = QApplication([])
-    window = Main()
-    app.exec_()
+    window = MirrorUI()
     
-    asyncio.run(window.setDateTime())
-    asyncio.run(window.setTemp())
+    # asyncio.run(window.setDateTime())
+    # asyncio.run(window.setTemp())
+
+    app.exec_()
