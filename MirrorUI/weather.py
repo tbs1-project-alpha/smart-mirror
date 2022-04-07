d8081964a5d60017304891b77512e6e1265ee7a8
Justin Rosengart <j.rosengart@gmx.de>
2022-04-07 17:30:51 +0200

    changed

--- MirrorUI/weather.py
+++ MirrorUI/weather.py
@@ -6,7 +6,7 @@ from geotracker import *
 
 class Weather:
     def __init__(self) -> None:
-        api_key = "c952d2356d192376967582488a306668"
+        api_key = "API_KEY"
         lat = GeoTracker().getLatlng()[0]
         lon = GeoTracker().getLatlng()[1]
         self.url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={api_key}&units=metric"
